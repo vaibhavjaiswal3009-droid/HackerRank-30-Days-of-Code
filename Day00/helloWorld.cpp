a5129a7218b361a87b90838ddc94b601ce458dc0
vaibhavjaiswal3009 <vaibhavjaiswal3009@gmail.com>
2026-04-03 20:02:23 +0530

    Add Day 0 solution

---
+++ Day00/helloWorld.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    string s;
+    getline(cin,s);
+    cout<<"Hello, World."<<endl; 
+    cout<<s<<endl; 
+    return 0;
+}
